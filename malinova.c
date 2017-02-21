4b6f74346e8ab42db4a4e750b77596ea54c73ee2
Imag375 <anastasiy-mag@mail.ru>
2017-02-21 23:47:22 +0300

    доделала

--- malinova.c
+++ malinova.c
@@ -9,15 +9,19 @@ int main(int argc, char *argv[]){
     } a;
     FILE *file;
     int i = 0;
-    printf("File name: ");
-    //scanf("%s\n", &adr);
-    if (!(file = fopen(argv[1], "rb")))
-        return -1;
+    int j = -1;
+    while(j==(-1)){
+        if (!(file = fopen(argv[1], "rb"))){
+            printf("File name: ");
+            scanf("%s", &argv[1]);
+        }
+        else j = 0;
+    }
     while (fread(&a, sizeof(struct packet), 1, file)){
         printf(" %d  %s  %f \n", a.x, a.y, a.z);
         i++;
     }
-    printf("File size = %dbyte\n", sizeof(struct packet)*i);
+    printf("File size = %d byte\n", sizeof(struct packet)*i);
     fclose(file);
     return 0;
 }
