5fb4e18bc88cdc57027cce6203781afcfadea956
Andrey <avragerghost@gmail.com>
2017-02-22 05:10:48 +0300

    TryHard2

--- vishnyakov1laba.c
+++ vishnyakov1laba.c
@@ -2,35 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define _PathLength 255
-
-typedef struct {
-    int intnumber;
-    char string[_PathLength];
-    float fltnumber;
-} readfile;
+struct _file {
+    int inum;
+    char string[255];
+    float fnum;
+};
 
 int main(int argc, const char * argv[]) {
-    readfile *thing = malloc(sizeof(readfile));
-    char path[_PathLength];
-    FILE *file;
+    int right = 0;
+    struct _file rf;
+    char fpath[255];
+    FILE *f = NULL;
+    while(right==0){
     printf("Enter the path to the file\n");
-    fgets(path, _PathLength, stdin);
-    path[strlen(path)-1] = '\0';
-    if ((file = fopen(path, "r+b")) == NULL) {
-        printf("Path is incorrect or file does not exist\n");
-        return 0;
+    fgets(fpath, 255, stdin);
+    fpath[strlen(fpath)-1] = '\0';
+    if ((f = fopen(fpath, "r+b")) == NULL) perror ("Error 228");
+    else {
+        right = 1;
     }
-    
+    }
     int i = 0;
-    while (!feof(file)) {
-        fread(thing, sizeof(readfile), 1, file);
-        printf("%d %s %f\n", thing->intnumber, thing->string, thing->fltnumber);
+    while (!feof(f)) {
+        fread(&rf, sizeof(rf), 1, f);
+        printf("%d %s %f\n", rf.inum, rf.string, rf.fnum);
         i++;
     }
     
     printf("Element amount: %d\n", i-1);
-    printf("File size: %ld byte\n", ftell(file));
-    fclose(file);
+    printf("File size: %ld byte/-s\n", ftell(f));
+    fclose(f);
     return 0;
 }
