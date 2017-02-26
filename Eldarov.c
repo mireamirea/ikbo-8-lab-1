50cdf0050032df539b62513f6e50f884953588ba
TandNvsWorld <rastagrant21@gmail.com>
2017-02-26 03:22:36 +0300

    Add files via upload

--- Eldarov.c
+++ Eldarov.c
@@ -6,16 +6,29 @@ char string[255];
 float floatingpointnumber;
 };
 
-int main()
+int main(int argc, char** argv)
 {
+    char path[255];
     FILE *f;
     struct packet packet1;
-    if (! (f = fopen("data.dat", "rb")))
+    if (argc != 2) /*(! (f = fopen("data.dat", "rb")))*/
     {
-        return printf("Can't find file of can't open file!\nPlease, try again...");
+        printf("Enter the path to file (data.dat): ");
+        scanf("%s",path);
+    if (!(f = fopen(path, "rb")))
+        {
+            printf("File don't exist\n");
+            return 0;
+        }
+
+    } else
+    if (!(f = fopen(argv[1], "rb")))
+    {
+        printf("File don't exist\n");
     }
+
     int i = 0;
-    printf(" Num. of Str: \t            String:\t          Floating point number: \n");
+    printf("\n Num. of Str: \t            String:\t          Floating point number: \n");
     while(fread(&packet1, sizeof(struct packet), 1, f))
     {
         printf("      %d       | %s |     %f      |\n", packet1.x, packet1.string, packet1.floatingpointnumber);
@@ -24,5 +37,5 @@ int main()
     printf("-------------------------------------------------------------------------");
     printf("\n\t\t\t Filesize: %d\n", i * sizeof(struct packet));
     fclose(f);
-    return 0;
+    return 1;
 }
