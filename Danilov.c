091c5fcfb29b6bea19f817d867536432ccb6cb3a
Dima <dima.danilov.2012@mail.ru>
2017-05-23 12:23:49 +0300

    Add files via upload

--- Danilov.c
+++ Danilov.c
@@ -3,6 +3,7 @@
 
 int main(int argc, char *argv[])
 {
+    int size=0;
     struct dadaya{
 int c;
 char arr[255];
@@ -12,8 +13,12 @@ float fl;} str;
 
 	file = fopen(argv[1], "rb");
 
-    while (fread(&str, sizeof(struct dadaya), 1,file))
+    while (fread(&str, sizeof(struct dadaya), 1,file)){
 		printf("%d    %s    %f\n", str.c,str.arr,str.fl);
+		size=size+sizeof(struct dadaya);
+    }
     fclose(file);
+    printf("%d bytes", size);
     return 0;
+
 }
